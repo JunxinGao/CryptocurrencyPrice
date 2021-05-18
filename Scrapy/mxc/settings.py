70231ceb625593fabfc94b77219c40583ccc1cb7
junxin.gao <junxin.gao@outlook.com>
2021-05-18 15:14:14 +0800

    add mxc pipeline setting

--- Scrapy/mxc/settings.py
+++ Scrapy/mxc/settings.py
@@ -98,9 +98,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'Scrapy.pipelines.ScrapyPipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'mxc.pipelines.MxcPipeline': 500,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
