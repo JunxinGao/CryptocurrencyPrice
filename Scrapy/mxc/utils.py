382f4f3190849269b1cd54b283eef8e66617f1d4
junxin.gao <junxin.gao@outlook.com>
2021-05-18 14:32:03 +0800

    parse mongodb host from env

--- Scrapy/mxc/utils.py
+++ Scrapy/mxc/utils.py
@@ -13,6 +13,7 @@ from urllib import parse
 # const
 USER_ENV = 'MONGO_INITDB_ROOT_USERNAME'
 PW_ENV = 'MONGO_INITDB_ROOT_PASSWORD'
+HOST_ENV = 'MONGO_INITDB_ROOT_HOST'
 PROD_ENV = 'PROD_ENV'
 DB_NAME = 'mxc'
 TB_MARKET_TICKERS = 'market_tickers'
@@ -21,10 +22,11 @@ TB_MARKET_TICKERS = 'market_tickers'
 if os.environ.get(PROD_ENV):
     u = parse.quote_plus(os.environ.get(USER_ENV))
     p = parse.quote_plus(os.environ.get(PW_ENV))
-    db_url = f'mongodb://{u}:{p}@mongodb:27017/'
+    host = parse.quote_plus(os.environ.get(HOST_ENV))
+    db_url = f'mongodb://{u}:{p}@{host}:27017/'
 else:
-    u = parse.quote_plus('pdgzf')
-    p = parse.quote_plus('pdgzf_db')
+    u = parse.quote_plus(DB_NAME)
+    p = parse.quote_plus(DB_NAME)
     db_url = f'mongodb://{u}:{p}@localhost:27018/'
 
 
